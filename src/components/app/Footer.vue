2f90ade21fa39f208b026dc50dca4cec54a227e3
Tsyrkun <vladtsyrkun@gmail.com>
2021-08-05 23:16:30 +0300

    adding files

---
+++ src/components/app/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card
+    class="flex"
+    flat
+    tile
+  >
+    <v-card-title class="teal">
+      <strong class="subheading">Get connected with me on social networks!</strong>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        v-for="social in socials"
+        :key="social.icon"
+        class="mx-4"
+        dark
+        icon
+        target="_blank"
+        :href="social.url"
+      >
+        <v-icon size="24px">
+          {{ social.icon }}
+        </v-icon>
+      </v-btn>
+    </v-card-title>
+
+    <v-card-text class="py-2 white--text text-center">
+      {{ new Date().getFullYear() }} — <strong>Vlad Tsyrkun</strong>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data: () => ({
+    socials: [
+      {
+        icon: 'mdi-github-circle',
+        url: 'https://github.com/tsyrkun',
+      },
+      {
+        icon: 'mdi-linkedin',
+        url: 'https://www.linkedin.com/in/vlad-tsyrkun',
+      },
+    ],
+  }),
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
+import dayjs from 'dayjs';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill';
 
+Vue.prototype.$dayjs = dayjs;
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
